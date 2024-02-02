d4766898b96d8421bb968842bddef64ce61781af
Eduardo Scrobote <EduardoScroboteProfissional@gmail.com>
2024-02-02 09:01:30 -0300

    feat: add-banner

--- pages/Home/Home.tsx
+++ pages/Home/Home.tsx
@@ -1,18 +1,45 @@
 "use client";
 
+import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa";
 import useGetRandomAnime from "./hooks/useGetRandomAnime";
 
 function HomePage() {
   const { data, isLoading, refetch } = useGetRandomAnime();
 
-  const bgImg = data?.data.images.jpg.image_url;
+  const bgImg = data?.data.images.jpg.large_image_url;
 
   return (
     <div className="w-full h-screen flex bg-gradient-to-tr from-black via-slate-800 to-black">
       <div className="w-[70%] h-screen flex flex-col">
-        <div className={`w-full h-[50%] bg-[url(${bgImg})] text-white`}>
-          img+name+button(next / prev)
-          <button onClick={() => refetch}>Hey clica</button>
+        <div className="flex flex-row w-[100%] p-8 justify-center h-screen bg-no-repeat bg-contain bg-center">
+          <button className="text-white" type="button" onClick={() => refetch}>
+            <div
+              onClick={() => refetch}
+              className="hover:bg-slate-500 rounded-full w-16 h-16 flex justify-center items-center"
+            >
+              <FaLongArrowAltLeft />
+            </div>
+          </button>
+          {isLoading ? (
+            <div className="w-[80%] bg-no-repeat bg-center bg-loading"></div>
+          ) : (
+            <div
+              style={{ backgroundImage: `url("${bgImg}")` }}
+              className="w-[80%] bg-contain bg-no-repeat bg-center"
+            ></div>
+          )}
+          <button
+            className="text-white m-12"
+            type="button"
+            onClick={() => refetch()}
+          >
+            <div
+              onClick={() => refetch()}
+              className="hover:bg-slate-500 rounded-full w-16 h-16 flex justify-center items-center"
+            >
+              <FaLongArrowAltRight />
+            </div>
+          </button>
         </div>
         <div className="w-full h-[50%] flex">
           <div className="bg-yellow-400 w-[50%]">+info</div>
@@ -25,3 +52,10 @@ function HomePage() {
 }
 
 export default HomePage;
+
+{
+  /* <div style={{ backgroundImage: `url("${bgImg}")` }}>
+img+name+button(next / prev)
+<button onClick={() => refetch}>Hey clica</button>
+</div> */
+}
